6d152594235e1a8b5b71a020dc557523c9e13803
egpennington <53251137+egpennington@users.noreply.github.com>
2023-05-19 02:53:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,3 +33,38 @@ function total() {
     goal = 0
 }
 
+// Team 2 ====================================== 
+let saveEl2 = document.getElementById("save-el2")
+let countEl2 = document.getElementById("count-el2")
+let count2 = 0
+
+let totalEl2 = document.getElementById("total-el2")
+let goalEl2 = document.getElementById("goal-el2")
+let goal2 = 0
+
+
+// Shots taken
+function increment2() {
+    count2 += 1
+    countEl2.textContent = count2
+}
+
+function save2() {
+    let countStr2 = count2 + " - "
+    saveEl2.textContent += countStr2
+    countEl2.textContent = 0
+    count2 = 0
+}
+
+// Goals scored
+function score2() {
+    goal2 += 1
+    goalEl2.textContent = goal2
+}
+
+function total2() {
+    let countStr2 = goal2 + " - "
+    totalEl2.textContent += countStr2
+    goalEl2.textContent = 0
+    goal2 = 0
+}
